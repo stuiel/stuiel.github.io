fe25ddf0a09875ca6b85f88337514cd85ca1349c
stuiel <sdlockhart8@gmail.com>
2017-06-27 21:53:46 +0100

    (newerlayout)

--- js/js.js
+++ js/js.js
@@ -1,7 +1,17 @@
 
+$(document).ready(function () {
+    slider();
+});
 
+$(window).scroll(function () {
+    slider();
+});
 
+function slider() {
+    if (document.body.scrollTop > 100)
+        $('#slider').stop().animate({"margin-right": '60'});
 
-
-
+    else
+        $('#slider').stop().animate({"margin-right": '-200'});
+}
 
