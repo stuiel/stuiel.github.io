0549f5077cd5a1e79a826993c5220d2dcf910ade
stuiel <sdlockhart8@gmail.com>
2017-06-29 14:17:18 +0100

    (NavbarWorking)

--- js/js.js
+++ js/js.js
@@ -1,6 +1,22 @@
 
 $(document).ready(function () {
     slider();
+
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+    applyNavigation();
+
 });
 
 $(window).scroll(function () {
@@ -8,9 +24,83 @@ $(window).scroll(function () {
 });
 
 function slider() {
-    if (document.body.scrollTop > 100)
-        $('#slider').stop().animate({"margin-right": '60'});
+    if (document.body.scrollTop > 500)
+        $('#navbar-example').stop().animate({"margin-right": '0'});
 
     else
-        $('#slider').stop().animate({"margin-right": '-200'});
+        $('#navbar-example').stop().animate({"margin-right": '-250'});
+}
+
+function redirect()
+{
+    var url = "https://play.google.com/store/apps/developer?id=LDevs&hl=en";
+    window.location(url);
+}
+
+function applyNavigation()
+{
+    applyClickEvent();
+    applyNavigationFixForPhone();
+    applyScrollSpy();
+    applyStickyNavigation();
+}
+
+
+function applyNavigationFixForPhone()
+{
+    $('.navbar li a').click(function(event)
+    {
+        $('.navbar-collapse').removeClass('in').addClass('collapse');
+    });
+}
+
+function applyScrollSpy()
+{
+    $('#navbar-example').on('activate.bs.scrollspy', function()
+    {
+        window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+    });
+}
+
+
+
+function applyStickyNavigation()
+{
+    lnStickyNavigation = $('.scroll-down').offset().top + 20;
+
+    $(window).on('scroll', function()
+    {
+        stickyNavigation();
+    });
+
+    stickyNavigation();
+}
+
+function stickyNavigation()
+{
+    if($(window).scrollTop() > lnStickyNavigation)
+    {
+        $('body').addClass('fixed');
+    }
+    else
+    {
+        $('body').removeClass('fixed');
+    }
+}
+
+function applyClickEvent()
+{
+    $('a[href*=#]').on('click', function(e)
+    {
+        e.preventDefault();
+
+        if( $( $.attr(this, 'href') ).length > 0 )
+        {
+            $('html, body').animate(
+                {
+                    scrollTop: $( $.attr(this, 'href') ).offset().top
+                }, 400);
+        }
+        return false;
+    });
 }
